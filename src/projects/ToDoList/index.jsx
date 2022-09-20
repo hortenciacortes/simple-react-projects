b7247a2b7b5187bae5fd5104e649a94eea8504f9
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-09-20 10:41:23 -0300

    refactoring local storage

--- src/projects/ToDoList/index.jsx
+++ src/projects/ToDoList/index.jsx
@@ -1,25 +1,14 @@
 import { useState } from 'react';
 import { ListItem } from '../../components/ToDoList/ListItem';
+import { Storage } from '../../storage';
 import './styles.scss';
 
 export function ToDoList() {
-
-  const Storage = {
-    set(list) {
-      localStorage.setItem('tasksList', JSON.stringify(list));
-    },
-
-    get() {
-      return JSON.parse(localStorage.getItem('tasksList')) || [];
-    }
-  } 
-
   const [list, setList] = useState(Storage.get());
 
   function addNewTask(e) {
     e.preventDefault();
     const inputValue = document.querySelector('input');
-console.log(e)
     if (e.code === 'Enter' || e.code === 'NumpadEnter' || e._reactName === 'onClick') {
       if (inputValue.value !== '') {
         const newTask = inputValue.value;
@@ -36,7 +25,7 @@ console.log(e)
     }
   }
   
-  Storage.set(list)
+  Storage.set('tasksList', list)
   console.log(list)
 
   return (
