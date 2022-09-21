6df480891f0478ebc4f215583da026a415c80916
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-09-21 17:25:54 -0300

    code rules

--- src/projects/Stopwatch/index.jsx
+++ src/projects/Stopwatch/index.jsx
@@ -13,17 +13,16 @@ export function Stopwatch() {
     reset,
   } = useStopwatch({ autoStart: false });
 
-
   return (
     <section className='container'>
+
       <div className='title'>
         <img src='src/assets/stopwatch.png' alt='Imagem de uma mão segurando um relógio' />
         <h2>Cronômetro</h2>
       </div>
 
       <div className="contain-stopwatch">
-
-        <div className="card">
+        <div className="timer card">
           <span>{hours < 10 ? `0${hours}` : hours}</span>
           <span>: {minutes < 10 ? `0${minutes}` : minutes}</span>
           <span>: {seconds < 10 ? `0${seconds}` : seconds}</span>
@@ -36,8 +35,8 @@ export function Stopwatch() {
           }
           <button className="card" onClick={() => reset(false, false)}><ArrowCounterClockwise size={26} color="#04a0d9" weight="bold" /></button>
         </div>
-
       </div>
+
     </section>
   );
 }
